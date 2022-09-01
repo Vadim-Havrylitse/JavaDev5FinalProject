9745d4a78a4af192baa2d7831c08628c7c8dd59b
Vadim-Havrylitse <vfgavrilice@gmail.com>
2022-09-01 16:03:35 +0300

    Initial commit

---
+++ group3/src/main/java/com/GOIT/JavaDev5/group3/JavaDev5FinalProjectApplication.java
@@ -0,0 +1,13 @@
+package com.GOIT.JavaDev5.group3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDev5FinalProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaDev5FinalProjectApplication.class, args);
+	}
+
+}
