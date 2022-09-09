2edcb7b0e198cc9312108a99fe859e1278a18fd2
IllyaMaz <maznichenkoo16@gmail.com>
2022-09-09 18:38:25 +0300

    logick for share_error

--- src/main/java/com/example/demo/noteController/NoteController.java
+++ src/main/java/com/example/demo/noteController/NoteController.java
@@ -63,11 +63,14 @@ public class NoteController {
         noteService.copyLink(map,req,resp);
     }
 
-    @GetMapping("/share/*")
-    public ModelAndView getShareNote(HttpServletRequest req){
-        ModelAndView modelAndView = new ModelAndView("note_read");
-        String[] split = req.getRequestURI().split("/");
-        modelAndView.addObject("note",noteService.getNoteById(UUID.fromString(split[3])));
+    @GetMapping("/share/error")
+    public ModelAndView getShareError(){
+        ModelAndView modelAndView = new ModelAndView("share_error");
         return modelAndView;
+    }
+
+    @GetMapping("/share/*")
+    public ModelAndView getShareNote(HttpServletRequest req, HttpServletResponse resp){
+        return noteService.getSharePage(req,resp);
     }
 }
